aed2652fbd99d56394719ed23621e73b026b6504
LHenriquerios <henriquerios4@gmail.com>
2022-08-01 19:59:20 -0300

    fix: change message erros

--- src/errors/catalog.ts
+++ src/errors/catalog.ts
@@ -4,7 +4,7 @@ export enum ErrorTypes {
 }
 
 type ErrorResponseObject = { 
-  message: string;
+  error: string;
   httpStatus: number
 };
   
@@ -15,11 +15,11 @@ export type ErrorCatalog = {
   
 export const errorCatalog: ErrorCatalog = {
   EntityNotFound: {
-    message: 'Entity Not Found',
+    error: 'Object not found',
     httpStatus: 404,
   },
   InvalidMongoId: {
-    message: 'Id must be a 24 characters hexadecimal',
+    error: 'Id must be a 24 characters hexadecimal',
     httpStatus: 400,
   },
 };
